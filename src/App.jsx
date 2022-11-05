489f1d7207099e276f4843c2614e1374d0be061a
Nereida Rondon <nereida.rondon5@gmail.com>
2022-11-05 18:06:45 -0400

    removed unused  CSS file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
 import AppRouter from './Components/AppRouter';
 import AppNavBar from './Components/AppNavBar';
 
 export default function App() {
   return (
-    <div className="App">
+    <div>
       <AppNavBar />
 
       <AppRouter />
